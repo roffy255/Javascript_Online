f5ff2224b1cd799c805fda0a6f63ac1f27da13b6
roffy255 <roffy.joshi5225@gmail.com>
2020-05-29 22:25:55 +0545

    update

--- main/object.js
+++ main/object.js
@@ -1,34 +1,73 @@
 //object
 
 
-function add(a,b){
-    return a+b
-}
+// function add(a,b){
+//     return a+b
+// }
 
-function subtract(a,b){
-    return a-b
-}
+// function subtract(a,b){
+//     return a-b
+// }
 
-function multiply(a,b){
-    return a*b
-}
+// function multiply(a,b){
+//     return a*b
+// }
 
-var results = {};
+// var results = {};
+//ES6
+// function myObject(name,  age, height){
+//     this.name = name;
+//     this.age = age;
+//     this.height = height;
+// }
+// myObject.prototype.gender = "male"
 
-
-results.add = add(1,2);
-results.subtract = subtract(2,3);
-results.multiply = multiply(4,5);
-
-
-var result_array = new Array();
-
-result_array.push(add(1,2))
-result_array.push(subtract(1,2))
-result_array.push(multiply(1,2))
+// var person = new myObject("roshan", "23", "5|7");
+// console.log(person.gender)
 
 
+// results.add = add(1,2);
+// results.subtract = subtract(2,3);
+// results.multiply = multiply(4,5);
 
 
+// var result_array = new Array();
+
+// result_array.push(add(1,2))
+// result_array.push(subtract(1,2))
+// result_array.push(multiply(1,2))
+
+// function calculate(){
+//     var a = 1;
+//     var b = 2;
+//     function add(var1, var2){
+//         var d =30
+    
+//         return var1+var2
+        
+//     }
+
+//     return add(a,b);
+// }
+
+// // console.log(calculate())
+// var _ = require('lodash')
+// var obj = {
+//     name: "sharad",
+//     age: "24"
+// }
+
+// var tempObj = _.clone(obj)
+
+// tempObj.name = "gaire"                      ///mutation
+
+// console.log(tempObj)
+// console.log(obj)
+
+// var1 = _.clone()
 
 
+//hoisting
+// var name
+// name = "roshan"
+// console.log(name)
